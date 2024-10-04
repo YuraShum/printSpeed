452767ad7c16c4baedd64b1c19d402725502d2d9
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-10-04 17:03:57 +0300

    added a timer to control the time

--- client/src/components/TextInputSection/TextInputSection.jsx
+++ client/src/components/TextInputSection/TextInputSection.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { getRandomText } from '../../utils/getRandomText'
 import './TextInputSection.scss'
+import { IoIosRefresh } from "react-icons/io";
+import ResultSpeed from '../ResultSpeed/ResultSpeed';
 
 const TextInputSection = () => {
     const randomtext = getRandomText()
@@ -8,6 +10,21 @@ const TextInputSection = () => {
     const [currentInputText, setCurrentInputText] = useState('')
     const [isBlurred, setIsBlurred] = useState(false)
     const textareaRef = useRef(null)
+    const [wrongCounter, setWrongCounter] = useState(0); 
+    //!! пр розширені функціоналу можна дозволи токистувачу вибирати тривалість
+    const [timeLeft, setTimeLeft] = useState(60)
+
+    useEffect(() => {
+        if (!isBlurred && timeLeft > 0) {
+            const timer = setInterval(() => {
+                setTimeLeft(prevValue => prevValue - 1)
+            }, 1000)
+
+            return () => clearInterval(timer)
+        }
+    }, [timeLeft, isBlurred])
+    console.log(timeLeft)
+
 
     const adjustTextareaHeight = () => {
         const element = document.querySelector('textarea')
@@ -41,10 +58,11 @@ const TextInputSection = () => {
             if (input[i] === text[i]) {
                 correctText += input[i];
             } else {
-                break;
+                setWrongCounter( prevValue => prevValue + 1)
+                break
             }
         }
-    
+
         setCurrentInputText(correctText);
     };
 
@@ -55,32 +73,43 @@ const TextInputSection = () => {
     }
 
     return (
-        <div className='text-section'>
-            {isBlurred && (
-                <button
-                    onClick={handleResumeTyping}
-                    className='resume-button'>
-                    Продовжити друк
-                </button>
-            )}
-            <div className={`text-input-section ${isBlurred ? 'blurred' : ''}`}>
-                <p className='text-input-chars'>
-                    {text.split('').map((char, index) => (
-                        <span
-                            key={index}
-                            className={index < currentInputText.length ? 'correct' : ''}>
-                            {char}
-                        </span>
-                    ))}
-                </p>
-                <textarea
-                    ref={textareaRef}
-                    value={currentInputText}
-                    onChange={handleChangeInputText}
-                    readOnly={isBlurred}
-                ></textarea>
-            </div>
-        </div>
+        <>
+            {timeLeft > 0 ?
+                <div className='text-section'>
+                    {isBlurred && (
+                        <button
+                            onClick={handleResumeTyping}
+                            className='resume-button'>
+                            Продовжити друк
+                        </button>
+                    )}
+                    {/** input text section */}
+                    <div className={`text-input-section ${isBlurred ? 'blurred' : ''}`}>
+                        <p className='text-input-chars'>
+                            {text.split('').map((char, index) => (
+                                <span
+                                    key={index}
+                                    className={index < currentInputText.length ? 'correct' : ''}>
+                                    {char}
+                                </span>
+                            ))}
+                        </p>
+                        <textarea
+                            ref={textareaRef}
+                            value={currentInputText}
+                            onChange={handleChangeInputText}
+                            readOnly={isBlurred}
+                        ></textarea>
+                    </div>
+                    {/** timer section */}
+                    <div className='timer'>
+                        <IoIosRefresh  />
+                        <p>{timeLeft} сек</p>
+                    </div>
+                </div>
+                : 
+                <ResultSpeed numberCharactersEntered={currentInputText.length} numberOfWrongCharacters={wrongCounter}/>}
+        </>
     )
 }
 
