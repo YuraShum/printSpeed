c370f75d2eeb33da08ee9e1d3d865070c277a332
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-10-04 13:51:59 +0300

    input field optimized for better user experience

--- client/src/components/TextInputSection/TextInputSection.jsx
+++ client/src/components/TextInputSection/TextInputSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { getRandomText } from '../../utils/getRandomText'
 import './TextInputSection.scss'
 
@@ -6,12 +6,35 @@ const TextInputSection = () => {
     const randomtext = getRandomText()
     const [text, setText] = useState(randomtext)
     const [currentInputText, setCurrentInputText] = useState('')
+    const [isBlurred, setIsBlurred] = useState(false)
+    const textareaRef = useRef(null)
+
+    const adjustTextareaHeight = () => {
+        const element = document.querySelector('textarea')
+        element.style.height = 'auto';
+        element.style.height = paragraphElement().scrollHeight + 'px';
+    };
+
+    useEffect(() => {
+        adjustTextareaHeight()
+        const handleClick = () => {
+            setIsBlurred(true)
+        }
+        document.addEventListener('mousedown', handleClick)
+        return () => {
+            document.removeEventListener('mousedown', handleClick)
+        }
+    }, [])
+
+    
+
+    const paragraphElement = () => {
+        return document.querySelector('.text-input-chars')
+    }
 
     const handleChangeInputText = (event) => {
         const input = event.target.value
-
         let correctText = ''
-
         for (let i = 0; i < input.length; i++) {
             if (input[i] === text[i]) {
                 correctText += input[i]
@@ -20,26 +43,42 @@ const TextInputSection = () => {
                 break
             }
         }
-
         setCurrentInputText(correctText)
     }
-    console.log(text)
+
+    const handleResumeTyping = (event) => {
+        event.stopPropagation();
+        setIsBlurred(false)
+        textareaRef.current.focus()
+    }
 
     return (
         <div className='text-section'>
-            <p>
-                {text.split('').map((char, index) => (
-                    <span
-                        key={index}
-                        className={index < currentInputText.length ? 'correct' : ''}>
-                        {char}
-                    </span>
-                ))}
-            </p>
-            <textarea
-                value={currentInputText}
-                onChange={handleChangeInputText}>
-            </textarea>
+            {isBlurred && (
+                <button 
+                    onClick={handleResumeTyping}
+                    className='resume-button'>
+                    Продовжити друк
+                </button>
+            )}
+            <div className={`text-input-section ${isBlurred ? 'blurred' : ''}`}>
+                <p className='text-input-chars'>
+                    {text.split('').map((char, index) => (
+                        <span
+                            key={index}
+                            className={index < currentInputText.length ? 'correct' : ''}>
+                            {char}
+                        </span>
+                    ))}
+                </p>
+                <textarea
+                
+                    ref={textareaRef}
+                    value={currentInputText}
+                    onChange={handleChangeInputText}
+                    readOnly={isBlurred}
+                ></textarea>
+            </div>
         </div>
     )
 }
